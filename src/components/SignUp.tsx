62f8823d4a9fb1acd6666d9d41c885686d115e86
Aryan Pandey <margiaryan@gmail.com>
2024-11-17 20:41:32 +0530

    feat: SignUp

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -4,23 +4,46 @@ import { useState } from "react";
 function SignUp() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [errMsg, setErrMsg] = useState<string>("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios.post("http://localhost:3000/signup", {
-      username: username,
-      password: password,
-    });
+    await axios
+      .post("http://localhost:3000/signup", {
+        username: username,
+        password: password,
+      })
+      .catch((err) => {
+        setErrMsg(err.response.data);
+      });
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>Username:</label>
-        <input onChange={(e) => setUsername(e.target.value)} value={username} />
+        <input
+          type="text"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <br />
         <label>Password:</label>
-        <input onChange={(e) => setPassword(e.target.value)} value={password} />
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <br />
         <button type="submit">SignUp</button>
       </form>
+      <p>{errMsg}</p>
+      <p>
+        Already registered?
+        <br />
+        <a href="/login">Login</a>
+      </p>
     </>
   );
 }
